0d4bbbeb00d7f7ad5de2ec6a6e69ad09110228f3
Asrul Hanafi <hanafi.asrul@gmail.com>
2017-07-27 15:58:16 +0700

    Fix similarity measures

--- similarityMeasures.py
+++ similarityMeasures.py
@@ -3,6 +3,19 @@ import json
 from dbConn.db import Connect
 from autoAnswer.nlp import TextMining, RakeTags, WordNet
 
+ALLOWED_ORIGINS = ['http://localhost:4200']
+
+class CorsMiddleware(object):
+    def process_request(self, request, response):
+        origin = request.get_header('Origin')
+        if origin in ALLOWED_ORIGINS:
+            response.set_header('Access-Control-Allow-Origin', origin)
+            response.set_header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept")
+
+class Greeting:
+	def on_get(self, req, res):
+		res.body = json.dumps({'status': True, 'message': 'Welcome to auto answering'})
+
 class GetRecommendation:
 	def on_post(self, req, res):
 		tm = TextMining()
@@ -25,26 +38,36 @@ class GetRecommendation:
 			case += 'case when tags like "%%%s%%" then 1 else 0 end' % tag
 			like += 'tags like "%%%s%%"' % tag
 			i += 1
-		sql.execute("SELECT id, score, content, tags," + case + " as priority FROM question WHERE " + like + " order by priority desc limit 10")
+		sql.execute("SELECT id, id_answer, score, content, tags," + case + " as priority FROM question WHERE " + like + " order by priority desc limit 10")
 		
 		result = []
-		for id, score, content, tags, priority in sql.fetchall():
+		for id, id_answer, score, content, tags, priority in sql.fetchall():
+			sql.execute("SELECT content FROM answer WHERE id = '%s'", (id_answer,))
 			content = content.replace('\n', "")
+			answer = sql.fetchone()
+			if answer is None:
+				answer = 'Belum terjawab'
+			else:
+				answer = answer[0].replace('\n', ' ')
+
 			result.append({
+				'id': id,
 				'priority': priority, 
 				'similarity': wn.similarity(text, content),
 				'content': tm.stripTags(content),
+				'id_answer': id_answer,
+				'answer': tm.stripTags(answer)
 			})
 
 		result = sorted(result, key = lambda x: x['similarity'], reverse = True)
-		print('hello')
 		question = {
 			'target': text,
 			'result': result,
 		}
 
-		res.body = json.dumps(question)
+		res.body = json.dumps(result[:5])
  
-api = falcon.API()
-api.add_route('/', GetRecommendation())
+api = falcon.API(middleware=[CorsMiddleware()])
+api.add_route('/', Greeting())
+api.add_route('/getRecommendation', GetRecommendation())
 # waitress-serve --port=8080 similarityMeasures:api
